aaf544dfb4876c3bbd123ab6148bfa559ecdf171
Leonardo Euzebio <diverleosp@gmail.com>
2026-04-01 16:54:18 -0300

    add stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -6,9 +6,21 @@ import { sleep, check } from 'k6';
 export const options = {
   //quantidade de inteirações (no nosso exemlplo logins)
   //iterations: 50,
+  
   // testando vus (usuários virtuais)
-  vus: 10,
-  duration: '30s',
+  //vus: 10,
+  //duration: '30s',
+  
+  //testando stages (mudança no estagio do numero de vus)
+  stages: [
+      {duration: '10s', target: 10},
+      {duration: '20s', target: 10},
+      {duration: '10s', target: 30},
+      {duration: '20s', target: 30},
+      {duration: '20s', target: 0},
+
+
+  ],
   thresholds: {
     http_req_duration: ['p(90)<5', 'max<2'],
     http_req_failed: ['rate<0.01']
